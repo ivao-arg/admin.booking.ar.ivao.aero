1124ef4dba0a811e36847e6c98343aa3d75be5d9
Francisco Gonzalez <78659310+Fransua0206@users.noreply.github.com>
2024-02-21 03:25:18 -0300

    fix(env): set default to AR division

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 export const Env = {
-  API_HOST: process.env.REACT_APP_API_HOST || 'https://api.kronos.br.ivao.aero',
-  LOGO_URL: process.env.REACT_APP_LOGO_URL || 'https://assets.br.ivao.aero/logos/tag_white.png',
-  MAIN_SYSTEM_URL: process.env.REACT_APP_MAIN_SYSTEM_URL || 'https://kronos.br.ivao.aero',
+  API_HOST: process.env.REACT_APP_API_HOST || 'https://api.booking.ar.ivao.aero',
+  LOGO_URL: process.env.REACT_APP_LOGO_URL || 'https://files.ar.ivao.aero/PR/tag_white.svg',
+  MAIN_SYSTEM_URL: process.env.REACT_APP_MAIN_SYSTEM_URL || 'https://booking.ar.ivao.aero',
   DEV: Boolean(process.env.REACT_APP_DEV) || false,
 };
